57f0431faabe560507dc979e33101ac918d8686c
Leo Ho <44418284+leohohoho@users.noreply.github.com>
2021-12-11 22:05:29 -0500

    Update names

--- server.py
+++ server.py
@@ -1,41 +1,45 @@
+import socket
 import base64
 import hashlib
-import socket
-from Crypto import Random
-from Crypto import Util
-from Crypto.Cipher import AES
 import pyDHE
+from Crypto import Random
+from Crypto.Cipher import AES
+from Crypto import Util
 
-class AESCipher(object):
+class AESCipherGCM(object):
     def __init__(self, key): 
-        self.bs = AES.block_size
+        self.blockSize = AES.block_size
         self.key = hashlib.sha256(key).digest()
 
-    def encrypt(self, raw):
-        raw = self._pad(raw)
-        iv = Random.new().read(AES.block_size)
-        cipher = AES.new(self.key, AES.MODE_GCM, iv)
-        return base64.b64encode(iv + cipher.encrypt(raw.encode()))
-
-    def decrypt(self, enc):
-        enc = base64.b64decode(enc)
-        iv = enc[:AES.block_size]
-        cipher = AES.new(self.key, AES.MODE_GCM, iv)
-        return self._unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
-
-    def _pad(self, s):
-        return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
+    def _pad(self, payload):
+        padSize = self.blockSize - len(payload) % self.blockSize
+        return payload + chr(padSize) * padSize
 
     @staticmethod
-    def _unpad(s):
-        return s[:-ord(s[len(s)-1:])]
+    def _unpad(payload):
+        #length = self.blockSize - len(payload) % self.blockSize
+        #return payload[:length]
+        return payload[:-ord(payload[len(payload)-1:])]
+
+    def encrypt(self, plaintext):
+        plaintext = self._pad(plaintext)
+        initializationVector = Random.new().read(AES.block_size)
+        # Use AES-GCM for encryption
+        aes_gcm = AES.new(self.key, AES.MODE_GCM, initializationVector)
+        return base64.b64encode(initializationVector + aes_gcm.encrypt(plaintext.encode()))
+
+    def decrypt(self, ciphertext):
+        ciphertext = base64.b64decode(ciphertext)
+        initializationVector = ciphertext[:AES.block_size]
+        aes_gcm = AES.new(self.key, AES.MODE_GCM, initializationVector)
+        return self._unpad(aes_gcm.decrypt(ciphertext[AES.block_size:])).decode('utf-8')
 
 
 string="This is the email"
 
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind(('localhost', 4444))
+sock.bind(('localhost', 8080))
 sock.listen(5)
 print("Listening for connections...")
 
@@ -47,9 +51,9 @@ shared_key = password.negotiate(conn)
 finalKey = Util.number.long_to_bytes(shared_key)
 
 
-enc_string=str(AESCipher(finalKey).encrypt(string))
-enc_string_2=AESCipher(finalKey).encrypt(string)
-dec_string=str(AESCipher(finalKey).decrypt(enc_string_2))
+enc_string=str(AESCipherGCM(finalKey).encrypt(string))
+enc_string_2=AESCipherGCM(finalKey).encrypt(string)
+dec_string=str(AESCipherGCM(finalKey).decrypt(enc_string_2))
 #print("This is the password " + password)
 print("Decrypted string: " + string)
 print("Encrypted string: " + enc_string)
